49936edcf8dbb7b31fd3ab74f48248cd423c6366
elena-vladimirova <avtobusova@mail.ru>
2019-11-09 14:33:19 +0300

    3. Коммиты на решения задач.[#18069]

---
+++ chapter_001/src/main/java/StudentInfo.java
@@ -0,0 +1,6 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Владимирова Елена");
+        System.out.println("08-08-1984");
+    }
+}
---
+++ chapter_001/src/test/java/SqAreaTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SqAreaTest {
+
+    @Test
+    public void squareWhenParams4and1ThenResult1() {
+
+        double result = SqArea.square(4, 1);
+        double expected = 1;
+
+        Assert.assertEquals(expected,result,0.01);
+    }
+    @Test
+    public void squareWhenParams6and2ThenResult2() {
+
+        double result = SqArea.square(6, 2);
+        double expected = 2;
+
+        Assert.assertEquals(expected,result,0.01);
+    }
+}
