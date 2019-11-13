025c74a8bc72d7ce40db1737e0877cfa67d74b1f
elena-vladimirova <avtobusova@mail.ru>
2019-11-13 14:54:37 +0300

    Ипотека.[#195325]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    // amount - сумма, выданная по кредиту
+    // salary - годовой доход
+    // percent - процентная ставка по кредиту
+    public int year(int amount, int salary, double percent) {
+        int year = 1;
+        // сумма к выплате по кредиту
+        double creditSum = amount + amount * percent/100;;
+
+        while (creditSum > salary){
+            year++;
+            creditSum = creditSum - salary;
+            creditSum = creditSum + creditSum * percent/100;
+        }
+        return year;
+    }
+}
