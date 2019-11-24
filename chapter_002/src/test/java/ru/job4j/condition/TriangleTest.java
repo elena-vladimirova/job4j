a365f5f6ac293d1232e4617ce9e4f61b263d6ed0
elena-vladimirova <avtobusova@mail.ru>
2019-11-24 23:13:04 +0300

    Рефакторинг - Площадь треугольника.[#195454]

---
+++ chapter_002/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TriangleTest {
+
+    @Test
+    public void whenTriangleExists() {
+
+        Triangle t = new Triangle (new Point (0, 0), new Point (0,2), new Point (2,0));
+        double result = t.area();
+        double expected = 2;
+
+        Assert.assertEquals(expected, result, 0.01);
+    }
+    @Test
+    public void whenTriangleNotExists() {
+
+        Triangle t = new Triangle (new Point (0, 0), new Point (0,2), new Point (0,1));
+        double result = t.area();
+        double expected = -1;
+
+        Assert.assertEquals(expected, result, 0.01);
+    }
+}
