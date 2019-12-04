bcaa6409a45a03391e99638d31e125a5b91d564d
elena-vladimirova <avtobusova@mail.ru>
2019-12-04 21:57:24 +0300

    Шаблон проектирования - Стратегия.[#195431]

---
+++ chapter_002/src/main/java/ru/job4j/stragery/Triangle.java
@@ -0,0 +1,8 @@
+package ru.job4j.stragery;
+
+public class Triangle implements Shape {
+    @Override
+    public String draw() {
+        return "Triangle";
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.stragery;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PaintTest {
+    @Test
+    public void whenDrawSquare() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Square());
+        assertThat(
+                out.toString(),
+                is(
+                        new StringBuilder()
+                                .append("++++\n")
+                                .append("+  +\n")
+                                .append("+  +\n")
+                                .append("++++\r\n")
+                                .toString()
+                )
+        );
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Triangle());
+        assertThat(
+                out.toString(),
+                is("Triangle\r\n")
+        );
+        System.setOut(stdout);
+    }
+}
