34a5a946d2e94428d985cfee7fc043b4ad86e56c
elena-vladimirova <avtobusova@mail.ru>
2019-12-11 21:33:09 +0300

    Телефонный справочник на базе ArrayList[#195401]

---
+++ chapter_002/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Petr");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+    @Test
+    public void whenFindBySurame() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Arsentev");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("534872");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Bryansk");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+}
