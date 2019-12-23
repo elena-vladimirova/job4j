c22f32c7ba5d395b7ca83d73d4fa8ad13f4cd603
elena-vladimirova <avtobusova@mail.ru>
2019-12-23 23:16:20 +0300

    Отсортировать департаменты [#34352]. Исправления.

--- chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,17 +5,18 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int result = o2.compareTo(o1);
-        if (o1.length() != o2.length()) {
-            int size = Math.min(o1.length(), o2.length());
-            String o1Trunc = o1.substring(0, size);
-            String o2Trunc = o2.substring(0, size);
-            if (o1Trunc.equals(o2Trunc) && o1.length() > o2.length()) {
-                result = 1;
-            } else if (o1Trunc.equals(o2Trunc) && o1.length() < o2.length()) {
-                result = -1;
-            }
-        }
+        int result = 0;
+        if (o1.length() == o2.length()) {
+            result = o2.compareTo(o1);
+        } else {
+                 if (o1.startsWith(o2)) {
+                     result = 1;
+                 } else if (o2.startsWith(o1)) {
+                     result = -1;
+                 } else {
+                     result = o2.compareTo(o1);
+                 }
+               }
         return result;
     }
 }
