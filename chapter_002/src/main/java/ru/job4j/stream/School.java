2a34cced2453dacc7f0e4933a6d3e0010738e678
elena-vladimirova <avtobusova@mail.ru>
2019-12-26 21:17:36 +0300

    Преобразование List в Map.[#195284]

--- chapter_002/src/main/java/ru/job4j/stream/School.java
+++ chapter_002/src/main/java/ru/job4j/stream/School.java
@@ -1,11 +1,18 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
-    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+
+    public List<Student> getStudentsList(List<Student> students, Predicate<Student> predict) {
         return students.stream().filter(predict).collect(Collectors.toList());
     }
+
+    public Map<String, Student> getStudentsMap(List<Student> students) {
+        return students.stream().collect(Collectors.toMap(s -> s.getName(), s -> s));
+    }
+
 }
--- chapter_002/src/test/java/ru/job4j/stream/SchoolTest.java
+++ chapter_002/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -3,7 +3,9 @@ package ru.job4j.stream;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -19,7 +21,7 @@ public class SchoolTest {
         Student s4 = new Student(65);
         Student s5 = new Student(100);
         List<Student> list = Arrays.asList(s1, s2, s3, s4, s5);
-        List<Student> result = school.collect(list, s -> s.getScore() > 70 && s.getScore() <= 100);
+        List<Student> result = school.getStudentsList(list, s -> s.getScore() > 70 && s.getScore() <= 100);
         List<Student> expected = Arrays.asList(s5);
         assertThat(result, is(expected));
     }
@@ -32,7 +34,7 @@ public class SchoolTest {
         Student s4 = new Student(65);
         Student s5 = new Student(100);
         List<Student> list = Arrays.asList(s1, s2, s3, s4, s5);
-        List<Student> result = school.collect(list, s -> s.getScore() > 50 && s.getScore() <= 70);
+        List<Student> result = school.getStudentsList(list, s -> s.getScore() > 50 && s.getScore() <= 70);
         List<Student> expected = Arrays.asList(s3, s4);
         assertThat(result, is(expected));
     }
@@ -45,8 +47,21 @@ public class SchoolTest {
         Student s4 = new Student(65);
         Student s5 = new Student(100);
         List<Student> list = Arrays.asList(s1, s2, s3, s4, s5);
-        List<Student> result = school.collect(list, s -> s.getScore() >= 0 && s.getScore() <= 50);
+        List<Student> result = school.getStudentsList(list, s -> s.getScore() >= 0 && s.getScore() <= 50);
         List<Student> expected = Arrays.asList(s1, s2);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenGetStudentsMap() {
+        School school = new School();
+        Student s1 = new Student("Lena", 10);
+        Student s2 = new Student("Petr", 20);
+        List<Student> list = Arrays.asList(s1, s2);
+        Map<String, Student> result = school.getStudentsMap(list);
+        Map<String, Student> expected = new HashMap();
+        expected.put("Lena", s1);
+        expected.put("Petr", s2);
         assertThat(result, is(expected));
     }
 }
