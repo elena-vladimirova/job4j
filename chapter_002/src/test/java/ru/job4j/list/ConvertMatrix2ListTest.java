caa1853a5f6d91f2e51dd4fb19f9562194774709
elena-vladimirova <avtobusova@mail.ru>
2019-12-28 22:15:50 +0300

    Collection API Улучшения[#195288]

--- chapter_002/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
+++ chapter_002/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -26,9 +26,7 @@ public class ConvertMatrix2ListTest {
 
     @Test
     public void whenListOfArrays() {
-        List<int[]> list = new ArrayList();
-        list.add(new int[]{1, 2});
-        list.add(new int[]{3, 4, 5, 6});
+        List<int[]> list = List.of(new int[]{1, 2}, new int[]{3, 4, 5, 6});
         ConvertMatrix2List converter = new ConvertMatrix2List();
         List<Integer> result = converter.toList(list);
         List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6);
--- chapter_002/src/test/java/ru/job4j/map/UserConvertTest.java
+++ chapter_002/src/test/java/ru/job4j/map/UserConvertTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -16,12 +17,9 @@ public class UserConvertTest {
         User user1 = new User(1, "Elena", "Moscow");
         User user2 = new User(2, "Egor", "Omsk");
         User user3 = new User(3, "Freddie", "London");
-        List<User> listUser = Arrays.asList(user1, user2, user3);
+        List<User> listUser = List.of(user1, user2, user3);
         HashMap<Integer, User> result = new UserConvert().process(listUser);
-        HashMap<Integer, User> expect = new HashMap();
-        expect.put(1, user1);
-        expect.put(2, user2);
-        expect.put(3, user3);
+        Map<Integer, User> expect = Map.of(1, user1, 2, user2, 3, user3);
         assertThat(result, is(expect));
     }
 
