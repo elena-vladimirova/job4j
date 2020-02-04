a1f2fbae0100a09f6c61b6121e04e41f4cbb51c9
elena-vladimirova <avtobusova@mail.ru>
2020-02-04 21:32:32 +0300

    Сканирование файловой системы.[#219681]

--- junior_chapter_2/src/test/java/ru/job4j/io/AnalizyTest.java
+++ junior_chapter_2/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -12,7 +12,7 @@ public class AnalizyTest {
 
     @Test
     public void whenUnavailable() {
-        Analizy.unavailable("src\\main\\resources\\test_log.txt","src\\main\\resources\\unavailable.csv");
+        Analizy.unavailable("src\\main\\resources\\test_log.txt", "src\\main\\resources\\unavailable.csv");
         try (BufferedReader read = new BufferedReader(new FileReader("src\\main\\resources\\unavailable.csv"))) {
             assertThat("10:57:01;10:59:01", is(read.readLine()));
             assertThat("11:01:02;11:02:02", is(read.readLine()));
--- junior_chapter_2/src/test/java/ru/job4j/io/SearchTest.java
+++ junior_chapter_2/src/test/java/ru/job4j/io/SearchTest.java
@@ -13,10 +13,10 @@ public class SearchTest {
     @Test
     public void whenFiles() {
         List<File> expected = List.of(
-                new File ("C:\\Users\\6145~1\\AppData\\Local\\Temp\\Job4JTest\\File3.txt"),
-                new File ("C:\\Users\\6145~1\\AppData\\Local\\Temp\\Job4JTest\\Folder1\\Folder1File1.txt"),
-                new File ("C:\\Users\\6145~1\\AppData\\Local\\Temp\\Job4JTest\\Folder1\\Folder1File2.java"),
-                new File ("C:\\Users\\6145~1\\AppData\\Local\\Temp\\Job4JTest\\Folder2\\Folder2File1.java")
+                new File("C:\\Users\\6145~1\\AppData\\Local\\Temp\\Job4JTest\\File3.txt"),
+                new File("C:\\Users\\6145~1\\AppData\\Local\\Temp\\Job4JTest\\Folder1\\Folder1File1.txt"),
+                new File("C:\\Users\\6145~1\\AppData\\Local\\Temp\\Job4JTest\\Folder1\\Folder1File2.java"),
+                new File("C:\\Users\\6145~1\\AppData\\Local\\Temp\\Job4JTest\\Folder2\\Folder2File1.java")
         );
         assertThat(expected,
                    is(Search.files("Job4JTest", Set.of("txt", "java")))
