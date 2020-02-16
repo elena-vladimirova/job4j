b2a2ecbd8e400bb82d3d910f1e2b5f1f4a614300
elena-vladimirova <avtobusova@mail.ru>
2020-02-16 15:44:13 +0300

    Поиск файлов по критерию[#219690]

---
+++ junior_chapter_2/src/main/java/ru/job4j/io/search/Args.java
@@ -0,0 +1,62 @@
+package ru.job4j.io.search;
+
+public class Args {
+
+    enum SearchMode {REGEXP, FULLNAME};
+
+    private String directory;
+    private String name;
+    private SearchMode mode = SearchMode.FULLNAME;
+    private String log;
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SearchMode getMode() {
+        return mode;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+    public Args(String[] args) {
+        String msg = "Correct arguments:\n" +
+                "-d search directory\n" +
+                "-n file name or regular expression\n" +
+                "-f search mode - complete match (default)\n" +
+                "-r search mode - regular expression\n" +
+                "-o result file";
+        int i = 0;
+        while (i < args.length) {
+            switch (args[i]) {
+                case ("-d"):
+                    directory = args[++i];
+                    break;
+                case ("-n"):
+                    name = args[++i];
+                    break;
+                case ("-f"):
+                    //mode = SearchMode.FULLNAME;
+                    break;
+                case ("-r"):
+                    mode = SearchMode.REGEXP;
+                    break;
+                case ("-o"):
+                    log = args[++i];
+                    break;
+                default:
+                    throw new IllegalArgumentException(msg);
+            }
+            i++;
+        }
+        if (directory == null || name == null || log == null) {
+            throw new IllegalArgumentException(msg);
+        }
+    }
+}
---
+++ junior_chapter_2/src/main/java/ru/job4j/io/search/Search.java
@@ -0,0 +1,55 @@
+package ru.job4j.io.search;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Predicate;
+
+/**
+ * 1. Создать программу для поиска файла.
+ * 2. Программа должна искать данные в заданном каталоге и подкаталогах.
+ * 3. Имя файла может задаваться, целиком, по маске, по регулярному выражение(не обязательно).
+ * 4. Программа должна собираться в jar и запускаться через java -jar find.jar -d c:/ -n *.txt -m -o log.txt
+ * Ключи
+ * -d - директория в которая начинать поиск.
+ * -n - имя файл, маска, либо регулярное выражение.
+ * -m - искать по макс, либо -f - полное совпадение имени. -r регулярное выражение.
+ * -o - результат записать в файл.
+ * 5. Программа должна записывать результат в файл.
+ * 6. В программе должна быть валидация ключей и подсказка.
+ */
+public class Search {
+
+    private Args args;
+
+    public Search(Args args) {
+        this.args = args;
+    }
+
+    public void search() throws IOException {
+        Path path = Paths.get(args.getDirectory());
+        Predicate<String> predicate;
+        if (args.getMode() == Args.SearchMode.REGEXP) {
+            predicate = n -> n.matches(args.getName());
+        } else {
+            predicate = n -> n.equals(args.getName());
+        }
+        try (PrintWriter out = new PrintWriter(args.getLog())) {
+            Files.walk(path)
+                 .filter(p -> Files.isRegularFile(p))
+                 .filter(f -> predicate.test(f.getFileName().toString()))
+                 .forEach(f -> out.println(f.toAbsolutePath().toString()));
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        // -d C:\projects\job4j\junior_chapter_2 -n Search.java -o C:\projects\job4j\junior_chapter_2\src\main\java\ru\job4j\io\search\log.txt
+        // -d C:\projects\job4j\junior_chapter_2 -n .*ip\.java -r -o C:\projects\job4j\junior_chapter_2\src\main\java\ru\job4j\io\search\log.txt
+        Args arguments = new Args(args);
+        Search search = new Search(arguments);
+        search.search();
+    }
+}
