1c8fc2eb11b0752e4f288719fbd72237e120f813
elena-vladimirova <avtobusova@mail.ru>
2020-04-05 22:17:07 +0300

    класс Count и метод int incremant()[#264881]. Исходный кодИсправления

--- middle_chapter_1/src/main/java/ru/job4j/concurrent/ParseFile.java
+++ middle_chapter_1/src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -1,8 +1,10 @@
 package ru.job4j.concurrent;
 
 import java.io.*;
+import java.util.stream.Collectors;
 
 public class ParseFile {
+
     private File file;
 
     public synchronized void setFile(File f) {
@@ -14,31 +16,35 @@ public class ParseFile {
     }
 
     public String getContent() throws IOException {
-        InputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            output += (char) data;
+        try (BufferedReader i = new BufferedReader(new FileReader(this.getFile()))) {
+            return i.lines().collect(Collectors.joining());
         }
-        return output;
     }
 
     public String getContentWithoutUnicode() throws IOException {
-        InputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            if (data < 0x80) {
-                output += (char) data;
+        try (BufferedReader i = new BufferedReader(new FileReader(this.getFile()))) {
+            StringBuffer output = new StringBuffer();
+            int data;
+            while ((data = i.read()) > 0) {
+                if (data < 0x80) {
+                    output.append((char) data);
+                }
             }
+            return output.toString();
         }
-        return output;
     }
 
     public void saveContent(String content) throws IOException {
-        OutputStream o = new FileOutputStream(file);
-        for (int i = 0; i < content.length(); i += 1) {
-            o.write(content.charAt(i));
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(this.getFile()))) {
+            out.print(content);
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ParseFile pf = new ParseFile();
+        pf.setFile(new File("middle_chapter_1\\src\\main\\resources\\ParseFile.txt"));
+        System.out.println(pf.getContent());
+        System.out.println(pf.getContentWithoutUnicode());
+        pf.saveContent("Hi!");
     }
 }
